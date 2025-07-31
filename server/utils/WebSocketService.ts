c827b00d3b97e53c1e111943f9962fef8ffc1835
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-07-31 17:39:43 +0200

    Updating websocket persistence

--- server/utils/WebSocketService.ts
+++ server/utils/WebSocketService.ts
@@ -1,5 +1,6 @@
 import { Server as HttpServer } from 'http';
 import { Server as SocketServer, Socket } from 'socket.io';
+import { DatabaseService } from './DatabaseService';
 
 export class WebSocketService {
   private io: SocketServer;
@@ -12,11 +13,21 @@ export class WebSocketService {
   }
 
   private handleConnections(): void {
+    const dbService = DatabaseService.getInstance(process.env.SQLITE_FILE!);
+
     this.io.on('connection', (socket: Socket) => {
       console.log(`Client connected: ${socket.id}`);
 
-      socket.on('message', (msg) => {
-        this.io.emit('message', msg);
+      // Send message history
+      dbService.getMessages()
+        .then((history: Array<{ authorId: string; authorName: string; content: string; timestamp: number }>) => socket.emit('history', history))
+        .catch((err: Error) => console.error(err));
+
+      // Handle incoming message
+      socket.on('message', (msg: { authorId: string; authorName: string; content: string; timestamp: number }) => {
+        dbService.addMessage(msg.authorId, msg.authorName, msg.content, msg.timestamp)
+          .then(() => this.io.emit('message', msg))
+          .catch((err: Error) => console.error(err));
       });
 
       socket.on('disconnect', () => {
