71b3813d6c59ede80c8cf7d3536cc9c29540c63d
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-08-01 09:48:54 +0200

    Update logger with function to log an object

--- server/utils/Logger.ts
+++ server/utils/Logger.ts
@@ -2,7 +2,9 @@ export class Logger {
   static info(message: string): void {
     console.log(`[INFO] ${new Date().toISOString()} - ${message}`);
   }
-
+  static infoObj(message: string, obj: any): void {
+    console.log(`[INFO] ${new Date().toISOString()} - ${message} ;` + obj);
+  }
   static error(message: string): void {
     console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
   }
