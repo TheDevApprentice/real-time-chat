437f11e72d1f867f1e13deacd9410ec75834496b
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-08-03 16:18:27 +0200

    Configuring router

--- interface/Vue/src/App.vue
+++ interface/Vue/src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <Login />
+  <router-view />
   <PwaStatus />
 </template>
 
 <script setup lang="ts">
-import Login from './views/Login.vue'
 import PwaStatus from './components/PwaStatus.vue'
 </script>
 
--- interface/Vue/src/views/Home.vue
+++ interface/Vue/src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-
+    <Suspense>
+        <template #default>
+            <PageTemplate>
+                <h1>Home</h1>
+            </PageTemplate>
+        </template>
+        <template #fallback>
+            <LoadingOverlay />
+        </template>
+    </Suspense>
 </template>
 
 <script setup lang="ts">
-
+import PageTemplate from "../components/PageTemplate.vue";
+import LoadingOverlay from "../components/LoadingOverlay.vue";
 </script>
 
 <style scoped>
