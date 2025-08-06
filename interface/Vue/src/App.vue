22682fb775fbd09fcb28612815a8bbbc494afdbc
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-08-06 17:29:22 +0200

    Test front end store to know what device the client is

--- interface/Vue/src/App.vue
+++ interface/Vue/src/App.vue
@@ -6,7 +6,25 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useDeviceStore } from './stores/DeviceStore';
 // import PwaStatus from './components/PwaStatus.vue'
+
+const device = useDeviceStore();
+
+onMounted(() => {
+  console.log('DeviceStore.deviceType:', device.deviceType);
+  console.log('DeviceStore.isMobile:', device.isMobile);
+  console.log('DeviceStore.isTablet:', device.isTablet);
+  console.log('DeviceStore.isDesktop:', device.isDesktop);
+  console.log('DeviceStore.isTV:', device.isTV);
+  console.log('DeviceStore.screenWidth:', device.screenWidth);
+  console.log('DeviceStore.screenHeight:', device.screenHeight);
+  console.log('DeviceStore.orientation:', device.orientation);
+  console.log('DeviceStore.pixelRatio:', device.pixelRatio);
+  console.log('DeviceStore.isTouchDevice:', device.isTouchDevice);
+  console.log('DeviceStore.userAgent:', device.userAgent);
+});
 </script>
 
 <style scoped>
