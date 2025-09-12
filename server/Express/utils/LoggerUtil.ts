7bb16b17e32057ae38dd4b1a0bc1eb006fdb72df
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-09-12 19:16:59 +0200

    Update du logger

--- server/Express/utils/LoggerUtil.ts
+++ server/Express/utils/LoggerUtil.ts
@@ -6,7 +6,7 @@ export class Logger {
     console.log(`[WARN] ${new Date().toISOString()} - ${message}` + obj);
   }
   static infoObj(message: string, obj: any): void {
-    console.log(`[INFO] ${new Date().toISOString()} - ${message} ;` + obj);
+    console.log(`[INFO] ${new Date().toISOString()} - ${message} : ` + obj);
   }
   static error(message: string): void {
     console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
